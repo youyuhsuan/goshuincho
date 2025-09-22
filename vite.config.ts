26be94e1b02b990c508397f631a5dfa1e981c2e3
youyuhsuan <stellayou0118@gmail.com>
2025-09-22 21:29:45 +0800

    chore(config): add tailwindcss import to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,15 @@ import vueDevTools from "vite-plugin-vue-devtools";
 import Components from "unplugin-vue-components/vite";
 import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    tailwindcss(),
     Components({
       resolvers: [PrimeVueResolver()],
     }),
