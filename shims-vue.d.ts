733b5d82d53eb905048d6eb9ce5ba90e21c65503
youyuhsuan <stellayou0118@gmail.com>
2025-10-10 22:47:28 +0800

    feat: add TypeScript declaration for SVG component imports
    Add type definition in shims-vue.d.ts to support importing SVG files as components using vite-svg-loader's ?component query parameter.

--- shims-vue.d.ts
+++ shims-vue.d.ts
@@ -5,7 +5,6 @@
 // Enables TypeScript to recognize .vue file imports
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
-
   const component: DefineComponent<object, object, unknown>;
   export default component;
 }
@@ -13,6 +12,12 @@ declare module "*.vue" {
 // SVG module declarations for vite-svg-loader
 declare module "*.svg" {
   import type { DefineComponent } from "vue";
+  const component: DefineComponent;
+  export default component;
+}
+
+declare module "*.svg?component" {
+  import { DefineComponent } from "vue";
   const component: DefineComponent;
   export default component;
 }
