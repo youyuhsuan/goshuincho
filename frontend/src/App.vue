a3846c358bf9ebff43b7db47eb5dd50a764d1881
youyuhsuan <stellayou0118@gmail.com>
2025-11-08 15:14:11 +0800

    refactor(style): extract navigation into Nav component
    - Move header and navigation markup from App.vue to Nav coponent
    - Import Nav and Footer component in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,11 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
-  <header class="mx-auto max-w-7xl px-6 lg:px-8 pt-8">
-    <nav class="flex items-center justify-between">
-      <RouterLink to="/">
-        <img
-          alt="Vue logo"
-          class="logo"
-          src="@/assets/logo.svg"
-          width="32"
-          height="32"
-        />
-      </RouterLink>
-      <div class="flex items-center gap-x-6">
-        <RouterLink class="flex group/item" to="/about">About</RouterLink>
-        <RouterLink class="flex group/item" to="/auth">Login</RouterLink>
-      </div>
-    </nav>
-  </header>
+  <Nav />
   <RouterView />
+  <Footer />
 </template>
+
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import Nav from "@/components/Nav.vue";
+import Footer from "@/components/Footer.vue";
+</script>
