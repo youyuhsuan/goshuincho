2491188eb08243b8cd42a69a5cd6b92bc17ea6ed
youyuhsuan <stellayou0118@gmail.com>
2026-01-10 20:55:38 +0800

    feat(ui): add footer component to main layout

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+// Components
+import Menubar from "@/components/Menubar.vue";
+import Footer from "@/components/Footer.vue";
+</script>
+
+<template>
+  <Menubar />
+  <RouterView />
+  <Footer />
+</template>
