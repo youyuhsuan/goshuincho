16800a6e12ea34ee63609919979aae6ef88a8f58
youyuhsuan <stellayou0118@gmail.com>
2026-03-23 15:17:24 +0800

    feat(settings): add Settinglayout with sidebar menu and slot
    - Add SettingLayout with computed menuItems ( Personal, Apperance, Billings) and a <slot> for injecting sub-page content
    - Use defineModel to sync active view state instead of vue-router, so each user's navigation state stays local and is not shared across via URL
    - Refactor SettingsView to delegate layout to SettingLayout via v-model:view, keeping the view layer thin

---
+++ frontend/src/components/setting/SettingLayout.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+// Primevue
+import Divider from "primevue/divider";
+// Types
+import type { SettingView } from "@/types/settingType";
+
+const view = defineModel<SettingView>("view");
+const menuItems = computed(() => [
+  {
+    label: "Personal",
+    command: () => (view.value = "personal"),
+  },
+  {
+    label: "Appearance",
+    command: () => (view.value = "appearance"),
+  },
+  {
+    label: "Billings",
+    command: () => (view.value = "billings"),
+  },
+]);
+</script>
+
+<template>
+  <div class="flex flex-1">
+    <!-- Menu -->
+    <aside class="shrink-0 self-stretch">
+      <Menu :model="menuItems" class="h-full" />
+    </aside>
+    <!-- Main layout -->
+    <main class="flex-1 px-8 py-10">
+      <div class="flex flex-col gap-1 mb-6">
+        <h1 class="text-xl font-bold text-surface-900 dark:text-surface-0">
+          Settings
+        </h1>
+        <span>Manage your account settings and preference.</span>
+        <Divider />
+      </div>
+      <slot></slot>
+    </main>
+  </div>
+</template>
---
+++ frontend/src/views/SettingsView.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+// Components
+import SettingLayout from "@/components/setting/SettingLayout.vue";
+// Type
+import type { SettingView } from "@/types/settingType";
+
+const view = ref<SettingView>("personal");
+</script>
+
+<template>
+  <SettingLayout v-model:view="view"> </SettingLayout>
+</template>
